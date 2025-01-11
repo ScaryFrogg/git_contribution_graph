35b79cfca8f333ac45f155435d3217a1d94d65bc
Vojin <puricvojin@gmail.com>
2025-01-11 01:06:42 +0100

    improved drawing

--- internal/draw/draw.go
+++ internal/draw/draw.go
@@ -5,34 +5,35 @@ import (
 )
 
 const (
-	resetColor = "\033[0m"
-	white      = "\x1b[48;5;249m"
-	c1         = "\x1b[48;5;40m"
-	c2         = "\x1b[48;5;34m"
-	c3         = "\x1b[48;5;28m"
-	c4         = "\x1b[48;5;22m"
+	resetColor    = "\033[0m"
+	invisibleText = "\033[8;31m"
+	white         = "\x1b[38;5;249m"
+	c1            = "\x1b[38;5;40m"
+	c2            = "\x1b[38;5;34m"
+	c3            = "\x1b[38;5;28m"
+	c4            = "\x1b[38;5;22m"
 )
 
-var colorMap = []string{resetColor, white, c1, c2, c3, c4}
+var colorMap = []string{white, c1, c2, c3, c4}
 
 func DrawGrid(activityLevels [][]int) {
 	for y := 0; y < len(activityLevels); y++ {
 		for x := 0; x < len(activityLevels[y]); x++ {
-			var colorIndex int
+			var colorCode string
 			cCount := activityLevels[y][x]
 			switch {
 			case cCount == -1:
-				colorIndex = 0
+				colorCode = invisibleText
 			case cCount == 0:
-				colorIndex = 1
+				colorCode = colorMap[0]
 			case cCount < 3:
-				colorIndex = 2
+				colorCode = colorMap[1]
 			case cCount < 5:
-				colorIndex = 3
+				colorCode = colorMap[2]
 			default:
-				colorIndex = 4
+				colorCode = colorMap[3]
 			}
-			fmt.Print(colorMap[colorIndex] + "  " + resetColor)
+			fmt.Print(colorCode + "⬛ " + resetColor)
 		}
 		fmt.Println()
 	}
